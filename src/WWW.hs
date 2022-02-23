f98cd2be006b92186b7c9b8e6f18520aeb2d5dc1
Pasqualino Titto Assini <tittoassini@gmail.com>
2022-02-23 14:02:18 +0100

    new formatting

--- src/WWW.hs
+++ src/WWW.hs
@@ -1,39 +1,42 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE TemplateHaskell       #-}
 
 module Main where
 
-import           Network.Wai.Handler.WarpTLS    ( runTLS
-                                                , tlsSettings
-                                                )
-import           Network.Wai.Handler.Warp       ( defaultSettings
-                                                , setPort
-                                                )
-import           Network.Wai.Middleware.RequestLogger
-                                                ( logStdoutDev )
-import           Web.Scotty
-import           Quid2.Util.Service
-import           System.Log.Logger
-import           System.Log.Handler.Simple
-import           Network.Wai.Middleware.StaticHost
-import           Network.Wai.Middleware.Autohead
-                                                ( autohead )
-import           Network.Wai.Middleware.Gzip
-import           Network.Wai.Middleware.AddHeaders
-import qualified Data.Text                     as T
-import           Control.Concurrent             ( forkIO )
-import           System.FilePath
-import           GitHash
-import           System.Posix.Daemonize
+import           Control.Concurrent                   (forkIO)
 -- import           Web.Scotty.TLS
-import           System.Directory
+
+import           Control.Exception
 import           Control.Monad
+import qualified Data.ByteString.Char8                as B8
+import qualified Data.Text                            as T
+import           GitHash
+import           Network.HTTP.Simple
+import           Network.Wai.Handler.Warp             (defaultSettings, setPort)
+import           Network.Wai.Handler.WarpTLS          (runTLS, tlsSettings)
+import           Network.Wai.Middleware.AddHeaders
+import           Network.Wai.Middleware.Autohead      (autohead)
+import           Network.Wai.Middleware.Gzip
+import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import           Network.Wai.Middleware.StaticHost
+import           ServiceOld
+import           System.Directory
+import           System.FilePath
+import           System.Log.Handler.Simple
+import           System.Log.Logger
+import           System.Posix.Daemonize
+import           Web.Scotty
+
+--
 
 -- TODO:  move in tapp
 versionID :: String
 -- versionID = concat [giBranch gi, "@", giHash gi, " (", giCommitDate gi, ")"]
 versionID = concat [giHash gi, " (", giCommitDate gi, ")"]
-  where gi = $$tGitInfoCwd
+ where
+  gi = $$tGitInfoCwd
 
 -- main = initService serviceName setup
 
@@ -64,7 +67,7 @@ main = do
   let appDev = logStdoutDev app
   -- app <- scottyApp $      get "/" (text "hello")
   let tlsConfig = tlsSettings certChain certKey
-      config    = setPort httpsPort defaultSettings
+      config = setPort httpsPort defaultSettings
   runTLS tlsConfig config appDev
 
 removeTempDir = do
@@ -78,6 +81,7 @@ requireFile fs = do
   when (not exists) $ error (unwords $ ["file", fs, "does not exists"])
 
 certChain = certFile "fullchain.pem"
+
 certKey = certFile "privkey.pem"
 
 certFile = (certDir </>)
@@ -86,14 +90,14 @@ certDir = "./private/certbot/quid2.org"
 
 -- certDir = "/etc/letsencrypt/live"
 httpServer =
-  middleware
-    $ gzip (def { gzipFiles = GzipCacheFolder tempDir })
-    . autohead
-    . staticHost hosts nonStaticPrefixes
-    . addHeaders allowHeaders
+  middleware $
+    gzip (def{gzipFiles = GzipCacheFolder tempDir})
+      . autohead
+      . staticHost hosts nonStaticPrefixes
+      . addHeaders allowHeaders
 
 allowHeaders =
-  [ ("Access-Control-Allow-Origin" , "*")
+  [ ("Access-Control-Allow-Origin", "*")
   , ("Access-Control-Allow-Headers", "Content-Type,Content-Length")
   ]
 
@@ -105,10 +109,10 @@ hosts =
   , h "jslib.quicquid.org"
   , h "massimoassini.quicquid.org"
   , h "ska.quicquid.org"
-    -- , h3 "finance.quicquid.org" "finance.quicquid.org" True -- PROB: NO LIVE UPDATE, NEED TO ADD finance
-  , h3 "quid2.com"    "quid2.org" False
-  , h3 "quid2.biz"    "quid2.org" False
-  , h3 "quid2.net"    "quid2.org" False
+  , -- , h3 "finance.quicquid.org" "finance.quicquid.org" True -- PROB: NO LIVE UPDATE, NEED TO ADD finance
+    h3 "quid2.com" "quid2.org" False
+  , h3 "quid2.biz" "quid2.org" False
+  , h3 "quid2.net" "quid2.org" False
   , h3 "quicquid.org" "quid2.org" False
   ]
  where
@@ -117,9 +121,7 @@ hosts =
   h2 n = h3 n n True
 
   h3 n n2 b =
-    Host { hostDomain = n, hostDir = T.concat [d, n2, "/web"], mutable = b }
+    Host{hostDomain = n, hostDir = T.concat [d, n2, "/web"], mutable = b}
 
   -- d = "/root/repo/workspace/" -- "/Users/titto/workspace/" -- "/home/workspace/"
   d = "./site/" -- "/Users/titto/workspace/" -- "/home/workspace/"
-
-
